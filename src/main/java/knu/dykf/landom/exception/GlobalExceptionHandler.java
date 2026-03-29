4579cd113ba040c80ebe7315cb66fff884f6d1bc
YoonGangHoon <yghun021007@naver.com>
2026-03-29 22:53:38 +0900

    Feat: 유효성 검사 에러 메시지 반환 기능 추가

--- src/main/java/knu/dykf/landom/exception/GlobalExceptionHandler.java
+++ src/main/java/knu/dykf/landom/exception/GlobalExceptionHandler.java
@@ -3,12 +3,30 @@ package knu.dykf.landom.exception;
 import knu.dykf.landom.dto.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+
+        String errorMessage = e.getBindingResult()
+                .getAllErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(ErrorCode.INVALID_INPUT_VALUE.getHttpStatus())
+                .body(new ErrorResponse(
+                        ErrorCode.INVALID_INPUT_VALUE.name(),
+                        errorMessage
+                ));
+    }
 
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
