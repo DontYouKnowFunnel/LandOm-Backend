ab539c93897eb85afcb5e85f05aee670ae1e1aac
YoonGangHoon <yghun021007@naver.com>
2026-03-31 23:35:37 +0900

    Chore: Swagger API 문서 설명 추가

--- src/main/java/knu/dykf/landom/controller/AnalyticsController.java
+++ src/main/java/knu/dykf/landom/controller/AnalyticsController.java
@@ -1,5 +1,7 @@
 package knu.dykf.landom.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import knu.dykf.landom.dto.response.FunnelResponse;
 import knu.dykf.landom.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,10 @@ public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
 
+    @Operation(summary = "퍼널 데이터 전송", description = "사용자 행동 데이터를 바탕으로 퍼널 이탈률을 전송합니다.")
     @GetMapping("/analytics/funnel")
     public ResponseEntity<FunnelResponse> getFunnelData(
+            @Parameter(description = "프로젝트 API 키", required = true)
             @RequestHeader("X-Project-Key") String projectKey) {
 
         FunnelResponse response = analyticsService.getScrollFunnelAnalytics(projectKey);
