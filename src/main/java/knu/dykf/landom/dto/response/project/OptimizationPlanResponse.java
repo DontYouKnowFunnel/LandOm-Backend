3cd695fc58f69db33912f6652951bce6394386ec
YoonGangHoon <yghun021007@naver.com>
2026-05-26 20:02:30 +0900

    Feat: 개선안 응답 구조 변경을 위한 엔티티 생성

--- src/main/java/knu/dykf/landom/dto/response/project/OptimizationPlanResponse.java
+++ src/main/java/knu/dykf/landom/dto/response/project/OptimizationPlanResponse.java
@@ -1,9 +1,12 @@
 package knu.dykf.landom.dto.response.project;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import knu.dykf.landom.dto.request.project.OptimizationRecommendation;
+
+import java.util.List;
 
 public record OptimizationPlanResponse(
-        @Schema(description = "HTML 개선안", example = "CTA 버튼 문구를 더 구체적으로 변경합니다.")
-        String optimizationPlan
+        @Schema(description = "HTML 개선안 목록")
+        List<OptimizationRecommendation> recommendations
 ) {
 }
